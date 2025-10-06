60f912eb894ed5784ca72bf5ecf041f8abf81fd8
Anika Lohia <anikalohia2@gmail.com>
2025-10-06 23:38:17 +0530

    changes added

--- controllers/taskContoller.js
+++ controllers/taskContoller.js
@@ -1,3 +1,11 @@
-export const createTask = (req,res)=>{
+export const createTask = async (req,res)=>{
+    let task=req.body.task;
+    let UserTask= await taskmodel.create({
+            title:task,
+            status:"pending",
+            user:"Anika"
     
+        })
+    console.log(UserTask);
+    res.status(201).json({success:true,message:"Task created successfully",task:UserTask});
 }
